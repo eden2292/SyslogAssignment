371b485cbc291c4048dc8f22c41e7eefcf1190f6
Eden Peacock <KP798@student.aru.ac.uk>
2023-10-21 15:53:23 +0100

    Initial commit with T6S3 radio class created.

---
+++ SyslogAssignment/Classes/TSixSeriesThree.cs
@@ -0,0 +1,11 @@
+﻿namespace SyslogAssignment.Classes;
+
+/// <summary>
+/// Object of T6S3 radio containing relevant information for sockets. 
+/// </summary>
+public class TSixSeriesThree
+{
+    private String? _iPAddress { get; set; }
+
+    private int ?_portNumber { get; set; }
+}
