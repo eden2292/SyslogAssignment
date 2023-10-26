d6eede620253197dd605b715639f4ccf1b333859
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2023-10-26 09:20:28 +0100

    Added interface for all radio items, good for expanding, ensures we can get port numbers, and relevant ip addresses from T6S3 function.

---
+++ SyslogAssignment/Classes/Interfaces.cs
@@ -0,0 +1,10 @@
+﻿using System;
+/// <summary>
+/// Interface used for all radio items that can be added, ensures code can be expanded at a later date.
+/// </summary>
+interface IRadio
+{
+  string GetIpv4Address();
+  int GetPort();
+  string GetIpv6Address();
+}
